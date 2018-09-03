cd882f3e9bfae0bcaa467c80c166a2b53a2dec1b
Brams Souare <bramssouare@hotmail.com>
2018-09-03 11:46:34 +0200

    Removed btnPostDelete class from show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -22,7 +22,7 @@
                 <div>
                     {!!Form::open(['action' => ['PostsController@destroy', $post->id, 'method' => 'POST', 'class' => 'pull-right']])!!}
                         {{Form::hidden('_method', 'DELETE')}}
-                        {{Form::submit('Delete', ['class' => 'btn btn-danger btnPostDelete'])}}
+                        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
                     {!!Form::close()!!}
                 </div>
             @endif
